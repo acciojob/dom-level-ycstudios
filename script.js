c4f344d637df8d06918c835015fab5b2d9cedfe0
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-21 18:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//your JS code here. If requirc
+const level=document.getElementById("level");
+const counter=1;
+while (element.parentElement) {
+	 element = element.parentElement;
+    level++;
+}
+alert("The level of the element is: " + level);
